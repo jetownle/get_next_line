e157da06481b35831aa07a36433fb5a2d429e396
Jeremy Townley <jetownle@e1z2r10p2.42.us.org>
2019-06-04 00:28:31 -0700

    byebye

--- libft/ft_copyuntil.c
+++ libft/ft_copyuntil.c
@@ -6,7 +6,7 @@
 /*   By: jetownle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 23:26:40 by jetownle          #+#    #+#             */
-/*   Updated: 2019/06/03 22:52:13 by jetownle         ###   ########.fr       */
+/*   Updated: 2019/06/04 00:28:03 by jetownle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,11 @@
 int	ft_copyuntil(char **dst, char *src, char c)
 {
 	int	i;
-	int	count;
 
 	i = -1;
-	count = 0;
 	while (src[++i])
 		if (src[i] == c)
 			break ;
 	*dst = ft_strndup(src, i);
-/*	if (!(*dst = ft_strnew(i)))
-		return (0);
-	while (src[count] && count < i)
-	{
-		if (!(*dst = ft_strjoinch(*dst, src[count])))
-			return (0);
-		count++;
-	}
-*/
 	return (i);
 }
