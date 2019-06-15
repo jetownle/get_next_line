a41eae9a92c840002f50868eb5bbe884c19c74b6
Jeremy Townley <jetownle@e1z2r3p35.42.us.org>
2019-06-14 22:29:57 -0700

    rescue me

--- get_next_line.c
+++ get_next_line.c
@@ -6,51 +6,61 @@
 /*   By: jetownle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 23:43:06 by jetownle          #+#    #+#             */
-/*   Updated: 2019/06/14 19:56:19 by jetownle         ###   ########.fr       */
+/*   Updated: 2019/06/14 21:51:49 by jetownle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	appendline(char **s, char **line, int fd)
+static int	appendline(char **s, char **line)
 {
-	char *tmp;
-	int i;
+	char	*tmp;
+	int		i;
 
 	i = 0;
-	while (s[fd][i] != '\n' && s[fd][i] != '\0')
+	while ((*s)[i] != '\n' && (*s)[i] != '\0')
 		i++;
-	if (s[fd][i] == '\n')
+	if ((*s)[i] == '\n')
 	{
-		*line = ft_strsub(s[fd], 0, i);
-		tmp = ft_strdup(&s[fd][i + 1]);
-		free(s[fd]);
-		s[fd] = tmp;
-		if (s[fd][0] == '\0')
+		*line = ft_strsub((*s), 0, i);
+		tmp = ft_strdup(&(*s)[i + 1]);
+		free(*s);
+		*s = tmp;
+		if ((*s)[0] == '\0')
 			ft_strdel(s);
 	}
 	else
 	{
-		*line = ft_strdup(s[fd]);
-		ft_strdel(&s[fd]);
+		*line = ft_strdup(*s);
+		ft_strdel(s);
 	}
 	return (1);
 }
 
-int	get_next_line(const int fd, char **line)
+static int	output(char **s, char **line, int ret, int fd)
 {
-	static char *s[4096];
-	char buf[BUFF_SIZE + 1];
-	char *tmp;
-	int ret;
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0 && s[fd] == NULL)
+		return (0);
+	else
+		return (appendline(&s[fd], line));
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char	*s[4096];
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
+	int			ret;
 
 	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
-			return (-1);
-	while((ret = read(fd, buf, BUFF_SIZE)))
+		return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
 		if (s[fd] == NULL)
-				s[fd] = ft_strdup(buf);
+			s[fd] = ft_strdup(buf);
 		else
 		{
 			tmp = ft_strjoin(s[fd], buf);
@@ -58,12 +68,7 @@ int	get_next_line(const int fd, char **line)
 			s[fd] = tmp;
 		}
 		if (ft_strchr(s[fd], '\n'))
-				break ;
+			break ;
 	}
-	if (ret < 0)
-		return (-1);
-	else if (ret == 0 && s[fd] == NULL)
-		return (0);
-	else
-		return (appendline(s, line, fd));
+	return (output(s, line, ret, fd));
 }
