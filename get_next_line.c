77267421e90a217992c788f218f50368a78f5a41
Jeremy Townley <jetownle@e1z2r7p17.42.us.org>
2019-06-14 19:32:17 -0700

    closer

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jetownle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 23:43:06 by jetownle          #+#    #+#             */
-/*   Updated: 2019/06/06 06:03:07 by jetownle         ###   ########.fr       */
+/*   Updated: 2019/06/14 19:29:29 by jetownle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,48 @@
 int	get_next_line(const int fd, char **line)
 {
 	static char *s[4096];
-	char		buf[BUFF_SIZE + 1];
-	char		*tmp;
-	int			ret;
-	int			i;
+	char buf[BUFF_SIZE + 1];
+	char *tmp;
+	int ret;
+	int i;
 
-	i = -1;
-	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
-		return (-1);
-	while ((ret = read(fd, buf, BUFF_SIZE)))
+	i = 0;	
+	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
+			return (-1);
+	while((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-		tmp = s[fd];
-		if (!(s[fd] = ft_strjoin(tmp, buf)))
-			s[fd] = ft_strdup(buf);
-		if (ft_strchr(buf, '\n'))
-			break ;
+		if (s[fd] == NULL)
+				s[fd] = ft_strdup(buf);
+		else
+		{
+			tmp = ft_strjoin(s[fd], buf);
+			free(s[fd]);
+			s[fd] = tmp;
+		}
+		if (ft_strchr(s[fd], '\n'))
+				break ;
 	}
-	if (ret < BUFF_SIZE && !ft_strlen(s[fd]))
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0 && s[fd] == NULL)
 		return (0);
-	while (s[fd][++i])
-		if (s[fd][i] == '\n')
-			break ;
-	*line = ft_strndup(s[fd], i);
-	(i < (int)ft_strlen(s[fd])) ? s[fd] += (i + 1) : ft_strclr(s[fd]);
+	else
+		while (s[fd][i] != '\n' && s[fd][i] != '\0')
+			i++;
+	if (s[fd][i] == '\n')
+	{
+		*line = ft_strsub(s[fd], 0, i);
+		tmp = ft_strdup(&s[fd][i + 1]);
+		free(s[fd]);
+		s[fd] = tmp;
+		if (s[fd][0] == '\0')
+				ft_strdel(s);
+	}
+	else
+	{
+		*line = ft_strdup(s[fd]);
+		ft_strdel(&s[fd]);
+	}
 	return (1);
 }
